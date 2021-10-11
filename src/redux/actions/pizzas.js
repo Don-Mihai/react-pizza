4cee994dbcffe031611c2aa18a6451213869dc59
mihai807 <mihai807@mail.ru>
2021-10-11 19:01:47 +0300

    fix proxy

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -4,7 +4,7 @@ export const fetchPizzas = (category, sortBy) => (dispatch) => {
   dispatch(setLoaded(false));
   axios
     .get(
-      `http://localhost:3001/pizzas?${
+      `/pizzas?${
         category !== null ? `category=${category}` : ''
       }&_sort=${sortBy}&_order=desc`,
     )
